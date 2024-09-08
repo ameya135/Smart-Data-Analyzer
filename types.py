6e777b3ce4042b5734ec0ba288b045f7acc59640
Viraj Shah <viraj.v.shah03@gmail.com>
2024-09-08 10:36:13 +0530

    Added response model for components

---
+++ types.py
@@ -0,0 +1,13 @@
+import pydantic
+
+
+class QueryProcessorResponseModel(pydantic.BaseModel):
+    user_prompt: str
+    database_query: str
+
+
+class QueryCheckerResponseModel(pydantic.BaseModel):
+    user_prompt: str
+    database_query: str
+    database_output: str
+    isValid: bool
